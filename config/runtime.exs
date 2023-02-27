952667b819fe39ce18b5dddcfcfa315ba6854c37
s-lando <slandolando@hotmail.com>
2023-02-26 16:56:39 -0800

    add runtime env var

--- config/runtime.exs
+++ config/runtime.exs
@@ -26,12 +26,7 @@ if config_env() == :prod do
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  secret_key_base = "dmhvtdjY/3avkXbxsbcy1SXa4u7q8TejuOGnhW4Kn1rdxG96cMBvfkdnh7ZF1/0E"
 
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
